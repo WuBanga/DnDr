b73d0ca139d1e5e23f3caa0bb489e060c8a37519
Eugene Volodin <wubanga.main@gmail.com>
2022-02-13 18:05:22 +0300

    feat: Add redirect after character create

--- src/pages/Create/Create.jsx
+++ src/pages/Create/Create.jsx
@@ -1,13 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+
 import { CharacterEditor } from '../../components/CharacterEditor/CharacterEditor';
 import { useCharacters } from '../../hooks/useCharacters';
 import { generateId } from '../../utils/generateId';
 
 export const Create = () => {
   const { addCharacter } = useCharacters();
+  const navigate = useNavigate();
   const saveCharacter = (character) => {
     const id = generateId();
     character['id'] = id;
     addCharacter(character);
+    setTimeout(() => {
+      navigate('/');
+    }, 1000);
   };
   return <CharacterEditor onSubmit={saveCharacter} />;
 };
