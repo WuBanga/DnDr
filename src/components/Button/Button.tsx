072607961a3179978a908ed4c0071aa74dc02fc7
Eugene Volodin <wubanga.main@gmail.com>
2022-03-30 18:31:50 +0300

    fix: Button component types

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,36 +2,35 @@ import { ComponentPropsWithRef, forwardRef } from 'react';
 
 import './Button.css';
 
-interface ButtonProps extends ComponentPropsWithRef<'button'> {}
+export const Button = forwardRef<
+  HTMLButtonElement,
+  ComponentPropsWithRef<'button'>
+>((props, ref) => {
+  const { type = 'button', className, color, ...otherProps } = props;
+  const classes = ['button'];
 
-export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  (props, ref) => {
-    const { type = 'button', className, color, ...otherProps } = props;
-    const classes = ['button'];
+  switch (color) {
+    case 'red':
+      classes.push('button--red');
+      break;
 
-    switch (color) {
-      case 'red':
-        classes.push('button--red');
-        break;
+    case 'green':
+      classes.push('button--green');
+      break;
 
-      case 'green':
-        classes.push('button--green');
-        break;
-
-      default:
-        break;
-    }
-
-    if (className) {
-      classes.push(className);
-    }
-    return (
-      <button
-        ref={ref}
-        className={classes.join(' ')}
-        type={type}
-        {...otherProps}
-      ></button>
-    );
+    default:
+      break;
   }
-);
+
+  if (className) {
+    classes.push(className);
+  }
+  return (
+    <button
+      ref={ref}
+      className={classes.join(' ')}
+      type={type}
+      {...otherProps}
+    ></button>
+  );
+});
